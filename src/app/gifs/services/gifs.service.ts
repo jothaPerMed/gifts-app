576ce597198915f0fda98b889203d072099a451e
jgperea <josegpermed@gmail.com>
2023-11-06 11:06:38 +0100

    05 Validaciones al servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class GifsService {
 
-  private _tagHistory : string [] = [];
+  private _tagHistory: string[] = [];
 
   constructor() { }
 
 
-  get tagHistory(){
+  get tagHistory() {
     return [...this._tagHistory];
   }
 
-  public searchTag(tag:string):void {
-    this._tagHistory.unshift(tag);
+  public searchTag(tag: string): void {
+    if (tag.length == 0) return;// Si la busqueda es vacia no se realiza
+    this.organizeHistory(tag);
     console.log(this.tagHistory);
+
+  }
+  organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+    // incluye los tags menos los repetidos
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory=this._tagHistory.filter((oldTag)=>oldTag!== tag);
+    }
+    this._tagHistory.unshift(tag);
+    // se utilizan namas que 10
+    this._tagHistory = this._tagHistory.splice(0, 10);
 
   }
 
